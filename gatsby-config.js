c7bacd4e7f047b60a4244a5c0e9d198c3c914bfd
moonti <tmmoond8@gmail.com>
2019-05-03 19:30:39 +0900

    필요 없는 page 제거 및 bluma, gatsby-plugin-purgecss 제거

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,13 +67,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
