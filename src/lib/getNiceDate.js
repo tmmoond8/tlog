1339eb8361056c8c996749dffa7d040134656e05
moonti <tmmoond8@gmail.com>
2019-05-12 22:00:48 +0900

    포스트 작성 시간 제대로 표현 못하는 것 수정

--- src/lib/getNiceDate.js
+++ src/lib/getNiceDate.js
@@ -1,26 +1,17 @@
 const niceFilter = [
-  { fn: Date.prototype.getYear, nice: "년" },
-  { fn: Date.prototype.getMonth, nice: "달" },
-  { fn: Date.prototype.getDate, nice: "일" },
-  { fn: Date.prototype.getHours, nice: "시간" },
-  { fn: Date.prototype.getMinutes, nice: "분" },
-  { fn: Date.prototype.getSeconds, nice: "초" },
+  { unit: 31536000000, nice: "년" },
+  { unit: 2592000000, nice: "달" },
+  { unit: 86400000, nice: "일" },
+  { unit: 3600000, nice: "시간" },
+  { unit: 60000, nice: "분" },
+  { unit: 1000, nice: "초" },
 ];
 
-const diff = (modifiedDate, today) => {
-  return (diffFn) => {
-    return diffFn.call(today) - diffFn.call(modifiedDate);
-  }
-}
-
-export default (date) => {
-  const lastModifiedDate = new Date(date);
-  const today = new Date();
-  const differ = diff(lastModifiedDate, today);
-
-  return niceFilter.reduce((accum, { fn, nice }) => {
+export default lastModifiedDate => {
+  const diffTime = new Date().getTime() - new Date(lastModifiedDate).getTime();
+  return niceFilter.reduce((accum, { unit, nice }) => {
     if(accum) return accum;
-    const d = differ(fn);
+    const d = Math.floor(diffTime / unit);
     return d === 0 ? null : `${d}${nice} 전` ;
   }, null);
 }
