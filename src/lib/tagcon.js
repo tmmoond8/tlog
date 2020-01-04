dab37aad055f40d726f6230352ae4004ab3353e5
moonti <tmmoond8@gmail.com>
2020-01-04 17:33:39 +0900

    posting about npm react module

--- src/lib/tagcon.js
+++ src/lib/tagcon.js
@@ -22,6 +22,7 @@ import book from "../img/tagcon/book.jpg";
 import seminar from "../img/tagcon/seminar.png";
 import html from "../img/tagcon/html.png";
 import svg from "../img/tagcon/svg.png";
+import npm from "../img/tagcon/npm.jpg";
 
 const TAGCON = {
   gatsby: { image: gatsby, name: "Gatsby.js" },
@@ -48,6 +49,7 @@ const TAGCON = {
   seminar: { image: seminar, name: "seminar" },
   html: { image: html, name: "html" },
   svg: { image: svg, name: "svg" },
+  npm: { image: npm, name: "npm" },
 }
 
 export const getImage = (tag) => {
