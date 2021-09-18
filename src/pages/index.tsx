852f19a0ee27546fb1c2494300df3145bfe3ea77
tmmoond8 <tmmoond8@gmail.com>
2021-09-18 13:04:42 +0900

    🚨 eslint, prettier 셋업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-);
+const Index = () => {
+  return (
+    <div>
+      <p>Hello Next.js</p>
+    </div>
+  );
+};
 
 export default Index;
