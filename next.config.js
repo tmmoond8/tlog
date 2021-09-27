12fd55c4fa64f17a93d05395ff725052bbc3cac2
tmmoond8 <tmmoond8@gmail.com>
2021-09-27 21:55:04 +0900

    🚨 sitema;

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/order */
 const dotenv = require('dotenv');
@@ -8,9 +10,13 @@ const withSvgr = require('next-plugin-svgr');
 dotenv.config();
 
 module.exports = withSvgr({
-  webpack: (_config) => {
+  webpack: (_config, { isServer }) => {
     const config = _config;
     config.plugins = config.plugins || [];
+
+    if (isServer) {
+      require(path.join(__dirname, 'scripts/generateSitemap.js'));
+    }
 
     config.plugins = [
       ...config.plugins,
