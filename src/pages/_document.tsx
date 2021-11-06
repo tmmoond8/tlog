1eb8368201d0221cc08dc818cad78db825d44ee2
tmmoond8 <tmmoond8@gmail.com>
2021-11-06 12:14:01 +0900

    👽️ 이제서야 html 언어 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class TlogDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="ko">
         <Head>
           <script
             async
